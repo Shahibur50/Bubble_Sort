c81203397c44eb417e4b0fbfe79b7ee00bf39bec
Shahibur Rahaman <72332158+Shahibur50@users.noreply.github.com>
2021-03-14 13:33:20 +0530

    Updates

---
+++ main.py
@@ -0,0 +1,34 @@
+'''
+Copyright (c) 2021, Shahibur Rahaman
+All rights reserved.
+'''
+
+list_of_numbers = [11, 15, 12, 1, 2, 3, 45, 4, 5, 6, 7, 8, 9, 10]
+
+
+def sort(sequence, reverse=False):
+    if reverse:
+        for _ in range(len(sequence) - 1):
+            for j in range(len(sequence) - 1):
+                element = sequence[j]
+                next_element = sequence[j + 1]
+                
+                if element < next_element:
+                    sequence[j], sequence[j + 1] = sequence[j + 1], sequence[j]  
+    else:
+        for _ in range(len(sequence)):
+            for j in range(len(sequence) - 1):
+                element = sequence[j]
+                next_element = sequence[j + 1]
+                
+                if element > next_element:
+                    sequence[j], sequence[j + 1] = sequence[j + 1], sequence[j]
+
+    return sequence
+
+
+print("# BUBBLE SORT")
+print()
+print(f"List sorted in Ascending order: {sort(list_of_numbers)}")
+print(f"List sorted in Descending order: {sort(list_of_numbers, reverse=True)}")
+print()
